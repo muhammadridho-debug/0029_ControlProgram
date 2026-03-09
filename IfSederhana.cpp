f5d1507b3c97c88950f018762afe737f778fce82
muhammadridho-debug <idomuhammad11@gmail.com>
2026-03-09 11:56:37 +0800

    menambahkan if statement dan display output

--- IfSederhana.cpp
+++ IfSederhana.cpp
@@ -7,4 +7,12 @@ int main()
 
     srand(time(0));
     x = rand() % 10;
+    
+    if (x == 5)
+    {
+        x = x + 1;
+        cout << "Nilai x sudah dikoreksi menjadi : " << x << endl;
+    }
+    cout << "Bilangan yang diberikan = " << x;
+
 }
